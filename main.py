afe31ba64e2f348aab1591bd7eb53f4c31f937d1
deldodai <dodaidzyan@gmail.com>
2023-05-12 08:16:55 +0200

    creation de la liste Question1

---
+++ main.py
@@ -0,0 +1,13 @@
+#I. creation de la liste des 10 elements
+print("I. Création d'une liste de 10 éléments de type chaîne de caractères")
+element_list=[
+    "Mercedes",
+    "BMW",
+    "Volkswagen",
+    "Mazda",
+    "Porche",
+    "Ford",
+    "Renaut",
+    "Citroen",
+    "Toyota",
+    "Opel"]
